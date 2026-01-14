30b99d2fe219f8d2fd2adfbecaa83eea5ad7361f
Tommadness <tom.m.cover@gmail.com>
2026-01-13 21:45:00 -0500

    Add assemblyinfo

---
+++ YookaArchipelago/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+using System.Reflection;
+using MelonLoader;
+using YookaArchipelago;
+
+[assembly: MelonInfo(typeof(YRAPMod), YookaArchipelago.BuildInfo.Name, YookaArchipelago.BuildInfo.Version, YookaArchipelago.BuildInfo.Author, YookaArchipelago.BuildInfo.DownloadLink)]
+[assembly: MelonColor(255,255,255,0)]
+
+// Create and Set up a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null,null)]
